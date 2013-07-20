4c6ff0fe2e376384bb7eac7bc8b4744d2082d6be
felix <felix@viennalabs.com>
2013-07-20 16:43:36 +0000

    populated index.html

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 	app.use(express.logger());
 	fs = require('fs');
-	
+
 app.get('/', function(request, response) {
   //response.send('Hello World!');
   fs.readFile('./static/index.html', function(err, data) {
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 	});
 });
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
